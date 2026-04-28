6bdabc4581d771d34efd706c147d321036393937
jackendychristopher-sketch <jackendychristopher@icloud.com>
2026-04-28 10:51:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const display = document.getElementById("display");
+
+function insert(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function Del() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Erreur";
+        setTimeout(clearDisplay, 1500);
+    }
+}
+
+function calculateSqrt() {
+    if (display.value !== "") {
+        try {
+            display.value = Math.sqrt(eval(display.value));
+        } catch (error) {
+            display.value = "Erreur";
+        }
+    }
+}
